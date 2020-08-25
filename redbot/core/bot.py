3267e529aa6fb7dae0d18f42a666dcbcc082ec6e
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-08-25 02:47:20 +0200

    the few things found in JackCogs

--- redbot/core/bot.py
+++ redbot/core/bot.py
@@ -27,6 +27,8 @@ from typing import (
     Any,
     Literal,
     MutableMapping,
+    Mapping,
+    Type,
     Set,
     overload,
     TYPE_CHECKING,
@@ -79,6 +81,7 @@ DataDeletionResults = namedtuple("DataDeletionResults", "failed_modules failed_c
 
 PreInvokeCoroutine = Callable[[commands.Context], Awaitable[Any]]
 T_BIC = TypeVar("T_BIC", bound=PreInvokeCoroutine)
+_CT = TypeVar("_CT", bound=commands.Context)
 UserOrRole = Union[int, discord.Role, discord.Member, discord.User]
 
 _ = i18n.Translator("Core", __file__)
@@ -100,6 +103,10 @@ class Red(
     commands.GroupMixin, RPCMixin, dpy_commands.bot.AutoShardedBot
 ):  # pylint: disable=no-member # barely spurious warning caused by shadowing
     """Our subclass of discord.ext.commands.AutoShardedBot"""
+
+    owner_id: None
+    owner_ids: Set[int]
+    cogs: Mapping[str, commands.Cog]
 
     def __init__(self, *args, cli_flags=None, bot_dir: Path = Path.cwd(), **kwargs):
         self._shutdown_mode = ExitCodes.CRITICAL
@@ -1278,7 +1285,7 @@ class Red(
     async def send_help_for(
         self,
         ctx: commands.Context,
-        help_for: Union[commands.Command, commands.GroupMixin, str],
+        help_for: commands.help.HelpTarget,
         *,
         from_help_command: bool = False,
     ):
@@ -1607,7 +1614,18 @@ class Red(
         for service in service_names:
             self.dispatch("red_api_tokens_update", service, MappingProxyType({}))
 
-    async def get_context(self, message, /, *, cls=commands.Context):
+    # apparently mypy can't infer the type from default
+    @overload
+    async def get_context(self, message: discord.Message, /) -> commands.Context:
+        ...
+
+    @overload
+    async def get_context(self, message: discord.Message, /, *, cls: Type[_CT] = ...) -> _CT:
+        ...
+
+    async def get_context(
+        self, message: discord.Message, /, *, cls: Type[_CT] = commands.Context
+    ) -> _CT:
         return await super().get_context(message, cls=cls)
 
     async def process_commands(self, message: discord.Message, /):
