9ea86fb3e0585fc379a68a60ef591eed09fa3183
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-04-09 17:04:08 +0200

    Update setuptools to 61.0+ and always set python_requires in setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from redbot import VersionInfo
 
 version, _ = VersionInfo._get_version(ignore_installed=True)
+python_requires = ">=3.8.1,<3.10"
 
 if os.getenv("TOX_RED", False) and sys.version_info >= (3, 10):
     # We want to be able to test Python versions that we do not support yet.
-    setup(python_requires=">=3.8.1", version=version)
-else:
-    # Metadata and options defined in setup.cfg
-    setup(version=version)
+    python_requires = python_requires.rsplit(",", maxsplit=1)[0]
+
+# Metadata and options defined in pyproject.toml
+setup(python_requires=python_requires, version=version)
