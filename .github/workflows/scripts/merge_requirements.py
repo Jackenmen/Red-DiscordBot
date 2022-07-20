d002b6cfadd4506693c71ff6f33d173a078dc6e6
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-07-20 04:50:25 +0200

    Simplify environment marker expressions

--- .github/workflows/scripts/merge_requirements.py
+++ .github/workflows/scripts/merge_requirements.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from packaging.markers import Marker
+from packaging.markers import Marker, Op, Value, Variable
 from packaging.requirements import Requirement
 
 
@@ -35,9 +35,10 @@ for name in names:
             platforms[platform_name] = req
 
     output = base_requirements if name == "base" else []
-    for req_name, platforms in input_data.items():
-        req = next(iter(platforms.values()))
-        if not all(value == req for value in platforms.values()):
+    for req_name, platform_data in input_data.items():
+        platforms = platform_data.keys()
+        req = next(iter(platform_data.values()))
+        if not all(value == req for value in platform_data.values()):
             raise RuntimeError(f"Incompatible requirements for {req_name}.")
 
         base_req = next(
@@ -58,20 +59,84 @@ for name in names:
         if len(platforms) == len(all_platforms):
             output.append(req)
             continue
-        elif len(platforms) < len(all_platforms - platforms.keys()):
+        elif len(platforms) < len(all_platforms - platforms):
             platform_marker = " or ".join(
                 f"sys_platform == '{platform}'" for platform in platforms
             )
         else:
             platform_marker = " and ".join(
-                f"sys_platform != '{platform}'" for platform in all_platforms - platforms.keys()
+                f"sys_platform != '{platform}'" for platform in all_platforms - platforms
             )
 
-        new_marker = (
-            f"({req.marker}) and ({platform_marker})"
-            if req.marker is not None
-            else platform_marker
-        )
+        if req.marker is None:
+            new_marker = platform_marker
+        else:
+            sys_platform_markers = set()
+            markers = []
+            for idx, marker in enumerate(req.marker._markers):
+                if marker == "or":
+                    new_marker = f"({req.marker}) and ({platform_marker})"
+                    break
+                if marker == "and":
+                    continue
+
+                if isinstance(marker, tuple) and marker[1].value in ("==", "!="):
+                    lhs, op, rhs = marker
+                    if isinstance(lhs, Variable):
+                        lhs_value = lhs.value
+                        rhs_value = rhs.value
+                    else:
+                        lhs_value = rhs.value
+                        rhs_value = lhs.value
+                    if lhs_value == "sys_platform":
+                        sys_platform_markers.add((lhs_value, op.value, rhs_value))
+                        continue
+
+                markers.append(marker)
+            else:
+                new_markers = []
+                for marker in markers:
+                    new_markers.append(marker)
+                    new_markers.append("and")
+
+                sys_platform_eq = None
+                sys_platform_ne = set()
+                for marker in sys_platform_markers:
+                    lhs, op, rhs = marker
+                    if op == "!=":
+                        sys_platform_ne.add(rhs)
+                        continue
+                    if sys_platform_eq is not None:
+                        raise RuntimeError(f"Unsatisfiable sys_platform markers for {req.name}")
+                    sys_platform_eq = rhs
+                if sys_platform_eq in sys_platform_ne:
+                    raise RuntimeError(f"Unsatisfiable sys_platform markers for {req.name}")
+
+                if sys_platform_eq is None:
+                    platforms -= sys_platform_ne
+                    if not platforms:
+                        continue
+                    if len(platforms) < len(all_platforms - platforms):
+                        platform_marker = " or ".join(
+                            f"sys_platform == '{platform}'" for platform in platforms
+                        )
+                    else:
+                        platform_marker = " and ".join(
+                            f"sys_platform != '{platform}'" for platform in all_platforms - platforms
+                        )
+                    new_markers.append(Marker(platform_marker)._markers)
+                    new_markers.append("and")
+                elif sys_platform_eq in platforms:
+                    new_markers.append((Variable("sys_platform"), Op("=="), Value(sys_platform_eq)))
+                    new_markers.append("and")
+                else:
+                    continue
+
+                if new_markers:
+                    new_markers.pop()
+                req.marker._markers = new_markers
+                new_marker = str(req.marker)
+
         req.marker = Marker(new_marker)
         if base_req is not None and base_req.marker == req.marker:
             continue
